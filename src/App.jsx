cf3691d6a56073487f259aaf72dffe06b8062064
dannybabu <danny1babu2@gmail.com>
2022-11-16 14:25:39 +0200

    Added logic and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState,useEffect } from 'react';
+import './App.css';
+import Separator from './assets/images/pattern-divider-desktop.svg';
+import Dice from './assets/images/icon-dice.svg';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setdata] = useState("");
+  const getquote = async()=>{
+    const url = 'https://api.adviceslip.com/advice';
+    const response = await fetch(url);
+    const data = await response.json();
+    setdata(data.slip.advice);
+  }
+
+  // useEffect 
+  useEffect(()=>{
+    getquote();
+  },[]);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="card">
+      <h1>{data}</h1>
+      <img className='seperator' src={Separator} alt="separator image"/>
+      <br />
+      <img className='dice' onClick={getquote} src={Dice} alt="dice"/>
     </div>
   )
 }
